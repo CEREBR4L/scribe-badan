7b34e1ece7017a3f0fd03ab02478982bbfb7a255
cerebral <mattmillergraphics@gmail.com>
2016-09-18 10:19:19 +0100

    Scoring fields added to collection

--- public/js/directives/storyDisplay/storyDisplay.js
+++ public/js/directives/storyDisplay/storyDisplay.js
@@ -7,3 +7,5 @@ angular.module('scribe')
 		};
 		
 	});
+
+	
--- routes/models/stories.js
+++ routes/models/stories.js
@@ -2,7 +2,19 @@ var mongoose = require('mongoose');
 
 var dbSchema = new mongoose.Schema({
 	title: String,
-	story: String
+	story: String,
+	positive: {
+		type: Number,
+		default: 0
+	},
+	negitive: {
+		type: Number,
+		default: 0
+	},
+	views: {
+		type: Number,
+		default: 0
+	}
 })
 
 module.exports = mongoose.model('stories', dbSchema);
