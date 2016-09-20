e8e8b664dda738f5443cdcf738ac0c9f72c64f21
CEREBR4L <mattmillergraphics@gmail.com>
2016-09-20 20:42:16 +0100

    Redesign and preview limit
    Another attempt at redesigning, there will be many more. As well as limiting the preview and fixing an issue with css not loading on some pages.

---
+++ public/js/directives/storyPreview/storyPreview.js
@@ -0,0 +1,11 @@
+angular.module('scribe')
+	.directive('storyPreview', function() {
+
+		return {
+			restrict: 'E',
+			templateUrl: 'js/directives/storyPreview/storyPreview.html'
+		};
+		
+	});
+
+	
