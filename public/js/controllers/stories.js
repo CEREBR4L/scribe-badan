02c83192eb75a03388d69529b932895a9f1bce6a
CEREBR4L <mattmillergraphics@gmail.com>
2016-09-29 20:36:51 +0100

    Can now up/downvote on the showcase page

--- public/js/controllers/stories.js
+++ public/js/controllers/stories.js
@@ -1,13 +1,45 @@
 
 angular.module('scribe')
-	.controller('stories', function storiesController($scope, getStories){
+	.controller('stories', function storiesController($scope, getStories, putUpvote, putDownvote){
 
-		getStories.getData()
-			.then(function(resp){
-	            
-	            $scope.stories = resp.data;
+		$scope.fetchStories = function(){
 
-	        })
+			getStories.getData()
+				.then(function(resp){
+		            
+		            $scope.stories = resp.data;
+
+		        })
+
+	    }
+
+	    $scope.addUpvote = function(id){
+
+			putUpvote.add(id)
+				.then(function(resp){
+					console.log("Upvote added!");
+					$scope.fetchStories();
+				})
+				.catch(function(e){
+					console.log("There was an error: " + e);
+				});
+
+		}
+
+		$scope.addDownvote = function(id){
+
+			putDownvote.add(id)
+				.then(function(resp){
+					console.log("Downvote added!");
+					$scope.fetchStories()
+				})
+				.catch(function(e){
+					console.log("There was an error: " + e);
+				});
+
+		}
+
+		$scope.fetchStories();
 
 	});
     
